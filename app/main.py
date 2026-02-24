a2d8dee6bc727aa202a007ae4ac652656ab809ca
MthunziD <mthunzimnyazi@gmail.com>
2026-02-24 12:20:45 +0200

    Flake8 issues sorted

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 class Car:
-    def __init__(self, comfort_class: int, clean_mark: int, brand: str):
+    def __init__(self, comfort_class: int, clean_mark: int,
+                 brand: str) -> None:
         self.comfort_class = comfort_class
         self.clean_mark = clean_mark
         self.brand = brand
@@ -7,7 +8,7 @@ class Car:
 
 class CarWashStation:
     def __init__(self, distance_from_city_center: float, clean_power: int,
-                 average_rating: float, count_of_ratings: int):
+                 average_rating: float, count_of_ratings: int) -> None:
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
         self.average_rating = average_rating
@@ -21,7 +22,7 @@ class CarWashStation:
                 self.wash_single_car(car)
         return total_cost
 
-    def calculate_washing_price(self, car):
+    def calculate_washing_price(self, car: Car) -> float:
         diff = 0
         price = 0
         diff = self.clean_power - car.clean_mark
@@ -32,11 +33,11 @@ class CarWashStation:
                      / self.distance_from_city_center)
         return round(price, 1)
 
-    def wash_single_car(self, car_obj):
+    def wash_single_car(self, car_obj: Car) -> None:
         if self.clean_power > car_obj.clean_mark:
             car_obj.clean_mark = self.clean_power
 
-    def rate_service(self, rate):
+    def rate_service(self, rate: int) -> None:
         self.average_rating = round((self.average_rating *
                                      self.count_of_ratings + rate) /
                                     (self.count_of_ratings + 1), 1)
